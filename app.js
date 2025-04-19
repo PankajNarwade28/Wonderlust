0f0d24fe5af7aca4e71e41afcf90a7492e3a0879
PankajNarwade28 <pankajnarwade258@gmail.com>
2025-04-19 21:55:06 +0530

    Adding Index,Show, Edit , Update Route

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const PORT = 2025;
-
+const Listing = require("./models/listings.js");
+const path = require("path");
 const MONGOURL = "mongodb://127.0.0.1:27017/wonderlust";
+
+// ejs setup
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// to access web data into express
+app.use(express.urlencoded({ extended: true }));
+
+// to serve static css and js files
+app.use(express.static(path.join(__dirname, "public")));
 
 async function main() {
   await mongoose.connect(MONGOURL);
@@ -18,9 +29,52 @@ main()
   });
 
 app.get("/", (req, res) => {
-  res.send("Root Directory");
+  // res.send("Root Directory");
+  res.render("sample.ejs");
 });
 
+// Index route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  // console.log(allListings);
+  res.render("listings/index.ejs", { allListings });
+});
+
+// Add New Route
+app.get("/listings/new", (req, res) => {
+  // res.send("Working");
+  res.render("listings/new.ejs");
+});
+
+// Post Listings
+app.post("/listings", (req, res) => {
+  // res.send("Working");
+  let listings = req.body.listing;
+  const newListing = new Listing(listings);
+  newListing.save().then(() => {
+    console.log("New Listing saved successfully!");
+  });
+  // console.log(listings);
+  res.redirect("/listings");
+});
+
+// Show route
+app.get("/listings/:id/", async (req, res) => {
+  let { id } = req.params;
+  // await Listing.findById(id).then((res) => {
+  //   console.log(res);
+  // });
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+// Edit Listing
+app.get("/listings/:id/edit", async (req, res) => {
+  // res.send("Working");
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
 app.listen(PORT, () => {
   console.log(`App is listening at http://localhost:${PORT}/`);
 });

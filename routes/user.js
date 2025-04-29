19bfe0f9bd62ac4bfef0fef7c4c8cf1ebcc82a5a
PankajNarwade28 <pankajnarwade258@gmail.com>
2025-04-30 00:23:14 +0530

    Added Authentication

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
+const passport = require("passport");
 
 // form render route
 router.get("/signup", (req, res) => {
@@ -12,13 +13,33 @@ router.get("/signup", (req, res) => {
 router.post("/signup", async (req, res) => {
   try {
     let { username, email, password } = req.body;
-    const newUser = await new User(username, email);
-    newUser.register(newUser, password);
-    console.log(newUser);
-    res.send("User is beign added");
+    const newUser = new User({ email, username });
+    const regUser = await User.register(newUser, password);
+    console.log(regUser);
+    // res.send("User is  added");
+    req.flash("success", "Welcome to Wonderlust");
+    res.redirect("/listings");
   } catch (er) {
-    console.log(er);
+    req.flash("error", er.message);
+    res.redirect("/signup");
   }
 });
+
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res) => {
+    req.flash("success", "Welcome Back! You are Logged in...");
+    // res.send("use login success");
+    res.redirect("/listings");
+  }
+);
 
 module.exports = router;

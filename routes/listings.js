26cf19623aa60d7603e1aa54720af8f67f895d36
PankajNarwade28 <pankajnarwade258@gmail.com>
2025-05-01 23:04:28 +0530

    Added MVC

--- routes/listings.js
+++ routes/listings.js
@@ -2,12 +2,15 @@ const express = require("express");
 const router = express.Router();
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
-const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+// app.js or server.js
+require("dotenv").config();
 // importing wrapAsync function to handle asynchronous errors
 const wrapAsync = require("../util/wrapAsync.js");
 // importing ExpressError to handle custom errors
 const ExpressError = require("../util/ExpressError.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 router
   .route("/")
